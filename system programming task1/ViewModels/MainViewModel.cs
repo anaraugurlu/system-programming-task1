d13a780d89e883d6ae3fe143d692244fbf45feed
admin <admin@DESKTOP-G2F4P4M>
2021-11-17 12:39:43 +0300

    Добавьте файлы проекта.

---
+++ system programming task1/ViewModels/MainViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using system_programming_task1.Command;
+
+namespace system_programming_task1.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+   
+        public MainWindow MainWindow { get; set; }
+        public ObservableCollection<Process> Processes { get; set; }
+        private Process sProcess;
+
+        public Process SProcess
+        {
+            get { return sProcess; }
+            set { sProcess = value; }
+        }
+   
+
+        public RelayCommand CreateBtn { get; set; }
+        public RelayCommand EndBtn { get; set; }
+
+        public MainViewModel(MainWindow window)
+        {
+
+            MainWindow = window;
+             
+            CreateBtn = new RelayCommand((sender => {
+                try
+                {
+
+                    Process.Start(window.processname .Text);
+                }
+                catch (Exception)
+                {
+
+                    MessageBox.Show("error");
+                }
+            
+                
+              
+                
+                  
+                
+            }));
+
+            EndBtn = new RelayCommand((sender =>
+            {
+                //try
+                //{
+                    
+                   // SProcess.Kill() ;
+
+                SProcess.Dispose();
+
+                //}
+                //catch (Exception )
+                //{
+                //    MessageBox.Show("error");
+                //}
+
+            }));
+        }
+
+    }
+}
